f75be535577a74408b18177044d629728d3bf0fd
Christopher Keith <chris.keith@gmail.com>
2022-08-08 10:44:35 -0700

    Add CSV header.

--- sql-to-csv.sh
+++ sql-to-csv.sh
@@ -1,11 +1,13 @@
-set echo
 # Edit output from PSQL pg_dump into CSV for import into Google Sheets.
-# Must manually add column headers.
+
 for f in $(ls *.sql) ; do
     tgt=$(echo $f | sed -e s/.sql/.csv/)
     echo $tgt
+    headers=$(cat $f | grep 'INSERT INTO' | head -1)
+    headers=$(echo $headers | sed -e s/') VALUES.*'//g | sed -e s/^.*'('//g)
+    echo $headers > $tgt
     cat $f | grep 'INSERT INTO' | \
         sed -e s/'^.* VALUES ('//g | \
         sed -e s/');'//g | \
-        sed -e "s/'/"'\"'/g > $tgt
+        sed -e "s/'/"'\"'/g >> $tgt
 done
