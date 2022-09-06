49c4aa18154a7d281c6487b2e8d79fa35e598fc1
Christopher Keith <chris.keith@gmail.com>
2022-09-06 14:04:31 -0700

    Add nvm use default

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,9 +14,10 @@ export NVM_DIR="$HOME/.nvm"                                 ; if [ $? -ne 0 ] ; 
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 nvm install 16.14.0                                         ; if [ $? -ne 0 ] ; then exit -6 ; fi
+nvm alias default node                                      ; if [ $? -ne 0 ] ; then exit -6 ; fi
 npm i yarn@^1.22.4 -g                                       ; if [ $? -ne 0 ] ; then exit -6 ; fi
 yarn run setup                                              ; if [ $? -ne 0 ] ; then exit -6 ; fi
 cd packages/client                                          ; if [ $? -ne 0 ] ; then exit -6 ; fi
-cp .env.example .env                                         ; if [ $? -ne 0 ] ; then exit -6 ; fi
+cp .env.example .env                                        ; if [ $? -ne 0 ] ; then exit -6 ; fi
 cd ../server                                                ; if [ $? -ne 0 ] ; then exit -6 ; fi
 cat .env.example | grep -v "^#." > .env                     ; if [ $? -ne 0 ] ; then exit -6 ; fi
