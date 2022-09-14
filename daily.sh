3906c9248c12e05ed309ca3f1df6c1cdff384d24
Christopher Keith <chris.keith@gmail.com>
2022-09-14 07:53:20 -0700

    start postgresql

--- daily.sh
+++ daily.sh
@@ -1,5 +1,8 @@
 # get latest, make sure it runs OK on my machine
+# May have to give sudo password.
+
 set -x
+sudo service postgresql start				            ; if [ $? -ne 0 ] ; then exit -6 ; fi
 git pull                                                ; if [ $? -ne 0 ] ; then exit -6 ; fi
 npx lerna bootstrap                                     ; if [ $? -ne 0 ] ; then exit -6 ; fi
 cd packages/server                                      ; if [ $? -ne 0 ] ; then exit -6 ; fi
