24e0933440ef16a5f6e39864655cd3ad535ff93c
Christopher Keith <chris.keith@gmail.com>
2022-09-14 08:03:15 -0700

    some fixes

--- daily.sh
+++ daily.sh
@@ -8,7 +8,7 @@ npx lerna bootstrap                                     ; if [ $? -ne 0 ] ; then
 cd packages/server                                      ; if [ $? -ne 0 ] ; then exit -6 ; fi
 yarn test                                               ; if [ $? -ne 0 ] ; then exit -6 ; fi
 yarn test:apis                                          ; if [ $? -ne 0 ] ; then exit -6 ; fi
-cd packages/client                                      ; if [ $? -ne 0 ] ; then exit -6 ; fi
+cd ../../packages/client                                ; if [ $? -ne 0 ] ; then exit -6 ; fi
 yarn test                                               ; if [ $? -ne 0 ] ; then exit -6 ; fi
 cd ../..                                                ; if [ $? -ne 0 ] ; then exit -6 ; fi
-yarn start:server
+yarn debug:server
