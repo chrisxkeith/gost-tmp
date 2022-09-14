69e76cbcbe68667bd991ac9d1aedc2c6c2b3c0cf
Christopher Keith <chris.keith@gmail.com>
2022-09-14 08:29:03 -0700

    start client also

--- daily.sh
+++ daily.sh
@@ -7,8 +7,15 @@ git pull                                                ; if [ $? -ne 0 ] ; then
 npx lerna bootstrap                                     ; if [ $? -ne 0 ] ; then exit -6 ; fi
 cd packages/server                                      ; if [ $? -ne 0 ] ; then exit -6 ; fi
 yarn test                                               ; if [ $? -ne 0 ] ; then exit -6 ; fi
+
+# Tests may fail first time around,
+#  probably due to the server cold-starting,
+# and test harness not waiting for it.
+# Just run it again.
+
 yarn test:apis                                          ; if [ $? -ne 0 ] ; then exit -6 ; fi
 cd ../../packages/client                                ; if [ $? -ne 0 ] ; then exit -6 ; fi
 yarn test                                               ; if [ $? -ne 0 ] ; then exit -6 ; fi
 cd ../..                                                ; if [ $? -ne 0 ] ; then exit -6 ; fi
+yarn start:client &
 yarn debug:server
