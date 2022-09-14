98de06ae3256e52a4641a713b2d89a67dd366bff
Christopher Keith <chris.keith@gmail.com>
2022-09-14 14:30:00 -0700

    Put comment before tests.

--- daily.sh
+++ daily.sh
@@ -6,13 +6,13 @@ sudo service postgresql start				            ; if [ $? -ne 0 ] ; then exit -6 ; 
 git pull                                                ; if [ $? -ne 0 ] ; then exit -6 ; fi
 npx lerna bootstrap                                     ; if [ $? -ne 0 ] ; then exit -6 ; fi
 cd packages/server                                      ; if [ $? -ne 0 ] ; then exit -6 ; fi
-yarn test                                               ; if [ $? -ne 0 ] ; then exit -6 ; fi
 
 # Tests may fail first time around,
 #  probably due to the server cold-starting,
 # and test harness not waiting for it.
 # Just run it again.
 
+yarn test                                               ; if [ $? -ne 0 ] ; then exit -6 ; fi
 yarn test:apis                                          ; if [ $? -ne 0 ] ; then exit -6 ; fi
 cd ../../packages/client                                ; if [ $? -ne 0 ] ; then exit -6 ; fi
 yarn test                                               ; if [ $? -ne 0 ] ; then exit -6 ; fi
