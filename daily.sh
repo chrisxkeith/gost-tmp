ff06249b87fdf18aacdf346c73e2cbee6a0c1d3d
Christopher Keith <chris.keith@gmail.com>
2022-09-14 14:46:23 -0700

    Skip non-existent client tests.

--- daily.sh
+++ daily.sh
@@ -14,8 +14,11 @@ cd packages/server                                      ; if [ $? -ne 0 ] ; then
 
 yarn test                                               ; if [ $? -ne 0 ] ; then exit -6 ; fi
 yarn test:apis                                          ; if [ $? -ne 0 ] ; then exit -6 ; fi
-cd ../../packages/client                                ; if [ $? -ne 0 ] ; then exit -6 ; fi
-yarn test                                               ; if [ $? -ne 0 ] ; then exit -6 ; fi
+
+# No real tests? "0 passing (0ms)""
+# cd ../../packages/client                                ; if [ $? -ne 0 ] ; then exit -6 ; fi
+# yarn test                                               ; if [ $? -ne 0 ] ; then exit -6 ; fi
+
 cd ../..                                                ; if [ $? -ne 0 ] ; then exit -6 ; fi
 yarn start:client &
 yarn debug:server
