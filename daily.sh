7ade7d3a59e6b74c0d56dfac3996746689a109d1
Christopher Keith <chris.keith@gmail.com>
2022-09-22 23:45:17 -0700

    Add git diff for temp fix.

--- daily.sh
+++ daily.sh
@@ -7,10 +7,15 @@ git pull                                                ; if [ $? -ne 0 ] ; then
 npx lerna bootstrap                                     ; if [ $? -ne 0 ] ; then exit -6 ; fi
 cd packages/server                                      ; if [ $? -ne 0 ] ; then exit -6 ; fi
 
-# Tests may fail first time around,
-#  probably due to the server cold-starting,
-# and test harness not waiting for it.
-# Just run it again.
+# If tests fail, make the following change:
+# --- a/packages/server/package.json
+# +++ b/packages/server/package.json
+#@@ -23,7 +23,7 @@
+#     "test": "yarn test:apis && yarn test:arpa && yarn test:db && yarn test:email",
+#     "test:email": "NODE_ENV=test mocha __tests__/email/*.test.js",
+#     "test:db": "NODE_ENV=test mocha --timeout 10000 __tests__/db/*.test.js",
+#-    "test:apis": "NODE_ENV=test SUPPRESS_EMAIL=TRUE node src > test.log & TEST_SERVER_PID=$!; NODE_ENV=test mocha --exit --bail --require __tests__/api/fixtures.js __tests__/api/*.test.js; TEST_RET_VAL=$?; kill $TEST_SERVER_PID; exit $TEST_RET_VAL",
+#+    "test:apis": "NODE_ENV=test SUPPRESS_EMAIL=TRUE node src > test.log & TEST_SERVER_PID=$!; sleep 10; NODE_ENV=test mocha --exit --bail --require __tests__/api/fixtures.js __tests__/api/*.test.js; TEST_RET_VAL=$?; kill $TEST_SERVER_PID; exit $TEST_RET_VAL",
 
 yarn test                                               ; if [ $? -ne 0 ] ; then exit -6 ; fi
 
