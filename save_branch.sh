906fce367c2b5c7f9f43eedfc301077e8afff50f
chrisxkeith <chris.keith@gmail.com>
2023-01-31 03:47:20 -0800

    latest

--- save_branch.sh
+++ save_branch.sh
@@ -1,32 +1,21 @@
 # Use as a template for copying off a branch and restoring it. Must run from usdr-gost/ .
 set -x
 
-git checkout 820_nathillardusdr_vscode_testdebug
+git checkout checkout 830-fix-grant-export-to-csv-2
 git pull
 
 for f in \
-.gitignore \
-packages/server/.vscode/launch.json \
-packages/server/.vscode/settings.json \
-packages/server/.vscode/tasks.json \
-packages/server/__tests__/arpa_reporter/server/fixtures/add-dummy-data.js \
-packages/server/__tests__/arpa_reporter/server/mocha_setup.sh \
-packages/server/__tests__/arpa_reporter/server/mocha_wrapper.sh \
-packages/server/__tests__/arpa_reporter/server/reset-db.sh \
-packages/server/package.json ; do
+packages/client/src/components/GrantsTable.vue \
+packages/server/__tests__/api/grants.test.js \
+packages/server/__tests__/db/db.test.js \
+packages/server/src/routes/grants.js ; do
     cp $f ..                                                    ; if [ $? -ne 0 ] ; then exit -6 ; fi
 done
 
-git checkout 830-fix-grant-export-to-csv
+git checkout 830-fix-grant-export-to-csv-2                      ; if [ $? -ne 0 ] ; then exit -6 ; fi
 git pull
 
-cp ../.gitignore .
-mkdir packages/server/.vscode/
-cp ../launch.json packages/server/.vscode/
-cp ../settings.json packages/server/.vscode/
-cp ../tasks.json packages/server/.vscode/
-cp ../add-dummy-data.js packages/server/__tests__/arpa_reporter/server/fixtures/
-cp ../mocha_setup.sh packages/server/__tests__/arpa_reporter/server/
-cp ../mocha_wrapper.sh packages/server/__tests__/arpa_reporter/server/
-cp ../reset-db.sh packages/server/__tests__/arpa_reporter/server/
-cp ../package.json packages/server/
+cp ../GrantsTable.vue packages/client/src/components/
+cp ../grants.test.js packages/server/__tests__/api/
+cp ../db.test.js packages/server/__tests__/db/
+cp ../grants.js packages/server/src/routes/
