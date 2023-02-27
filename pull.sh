9134e9a58ce41c05f38a688f7a2f312293fae74d
Christopher Keith <chris.keith@gmail.com>
2023-02-27 09:55:20 -0800

    Comment out 'clean'

--- pull.sh
+++ pull.sh
@@ -3,11 +3,11 @@
 date
 set -x
 git pull                                                ; if [ $? -ne 0 ] ; then exit -6 ; fi
-# To completely toss all docker images, volumes and containers, networks, images and volumes:
-docker-compose down --rmi all
+# To completely toss all docker images, volumes and containers, networks, images and volumes (can take some minutes):
+# docker-compose down --rmi all
 docker compose up -d                                    ; if [ $? -ne 0 ] ; then exit -6 ; fi
 docker compose exec app yarn install                    ; if [ $? -ne 0 ] ; then exit -6 ; fi
 docker compose exec frontend yarn install               ; if [ $? -ne 0 ] ; then exit -6 ; fi
 docker compose exec app yarn db:migrate                 ; if [ $? -ne 0 ] ; then exit -6 ; fi
 docker compose exec app yarn db:seed                    ; if [ $? -ne 0 ] ; then exit -6 ; fi
-# docker compose exec app yarn test                       ; if [ $? -ne 0 ] ; then exit -6 ; fi
+docker compose exec app yarn test                       ; if [ $? -ne 0 ] ; then exit -6 ; fi
